38c6e6e29cc075234aa6ff9984ab65fb039ad80c
Jorge Sisco <jorgesisco17@gmail.com>
2022-04-29 12:43:13 +0200

    Added 1st solved problem
    Next step is tu add the problem explanation

---
+++ leet_code/minimizeMemory.py
@@ -0,0 +1,31 @@
+def minimize(process, n, m):
+	record = {}
+
+	while n%2==0:
+		for i in range(0, n, m):
+			if process[i] == process[-1]:
+				break
+
+			record[process[i]] = process[i]+process[i+1]
+
+		del record[max(record, key=record.get)]
+
+		return sum(record.values())
+		break 
+
+	for i in range(0, n):
+		if process[i] == process[-1]:
+			break
+
+		record[process[i]] = process[i]+process[i+1]
+
+	del record[max(record, key=record.get)]
+
+
+	return sum(record.keys())
+
+process = [10, 4, 8, 1]
+n = len(process)
+m = 2
+
+minimize(process, n, m)
